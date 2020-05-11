cc493425378cf4cf86ae120ca2fdc91b3d403252
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-10 20:03:44 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,16 @@ class Song
         @@all
     end
 
+    def self.new_by_filename(file)
+        song_info = file.chomp(".mp3").split(" - ")
+        song = self.new(song_info[1])
+        song.artist_name = song_info[0]
+        song
+    end
+
     def artist_name=(name)
-        self.artist = name
-        self
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
     end
 
 end
