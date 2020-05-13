a011ca9c640866855e9d1cc142320e2363846f45
Philippe Timothe <philippetimothe@gmail.com>
2020-05-13 16:54:40 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,36 @@
+require 'pry'
 class Artist
   attr_accessor :name
   
   @@all = []
   
   def initialize(name) 
-     @name = name
-     @@all << self
+    @name = name
+    @@all << self
+    @songs = []
   end
   
   def self.all 
     @@all 
   end
   
+  def add_song(song)
+    @songs << song
+  
+  end
+  
+  def songs 
+    @songs 
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.all.detect {|instance| instance.name == name} ||
+        self.new(name)
+  end
+  
+  def print_songs
+    @songs.each{ |song_list| puts song_list.name}
+  end
   
   
   
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song 
   attr_accessor :artist, :name
   
@@ -12,8 +13,20 @@ class Song
     @@all 
   end 
   
-  def self.new_by_filename
+  def artist_name=(name)
+   self.artist = Artist.find_or_create_by_name(name)
+  end
+  
+  def self.new_by_filename(file)
+    filename = file.split(" - ")
+    artistname = filename[0]
+    songname = filename[1]
+    song = self.new(songname)
     
+
+    song = self.new(songname)
+    song.artist_name = artist_name
+    song
   end 
   
   
