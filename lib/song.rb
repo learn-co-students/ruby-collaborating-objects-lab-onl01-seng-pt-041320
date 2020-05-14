7d2c43e949d608dee18f481959b7860736543107
Joseph Romero <joeromero47@gmail.com>
2020-05-14 03:49:30 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def save 
+    @@all << self 
+    self 
+  end
+  
+  def self.new_by_filename(file_name)
+    song = file_name.chomp(".mp3").split(" - ")[1]
+    artist = file_name.chomp(".mp3").split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist 
+    new_song
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+end
